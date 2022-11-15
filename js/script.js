58f65223d1b956475948b31d501f4a5511ab9457
SianLyte <yaroslav_teplov@mail.ru>
2022-11-15 22:13:06 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strict";
+/* console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+number=10;
+console.log(number);
+
+console.log(example);
+example = 10;
+var example = 6;
+
+{
+    let exampleInsideContainer = 20;
+} 
+//console.log(exampleInsideContainer);
+//let нельзя переопределить и использовать до объявления, нельзя использовать вне контейнера, нельзя
+
+let number = 5;
+let string = "string";
+let obj = {
+    number: 5,
+    isError: false,
+    array: [],
+};
+let arr = ['bebra', 5, 6, 7, false, [[]]];
+let obj = {
+    1: 2,
+    2: 3,
+    5: "bebra",
+    IsArray: false,
+    s: 50
+};
+arrObj.isArray = false;
+console.log(obj['1']);
+console.log(obj.IsArray);
+console.log(arr[0]);
+console.log(obj.number);
+// let aaa=10;в
+obj.aaa=5; 
+console.log(obj['aaa'] + "понял");
+console.log(obj.aaa + "не понял");
+console.log(obj[1]);
+
+let arrr = [1,2,3];
+arrr[10] = false;
+console.log(arrr[10]);
+console.log(arrr); */
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = +prompt("Вам есть 18?","");
+//console.log(answer+5);
+//const answers = [];
+/* answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваша фамилия?", "");
+answers[2] = prompt("Как ваше отчество?", ""); */
+//console.log(typeof(answers));
+/* const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+const user = 'Ivan';
+console.log(`Привет, ${user}`); */
+let incr = 10,
+    decr = 10;
+//incr++;
+//decr--;
+console.log(incr++); //постфиксная форма сначала выводит значение, а только потом изменяет его,
+                    // при следующем вызове значение изменится
+console.log(decr--);
+
+console.log(++incr); //префиксная форма сразу меняет значение и выводит его
+console.log(--decr);
